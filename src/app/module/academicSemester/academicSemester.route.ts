5d35d9ab79f604877bedc4873df89d231e5499af
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-06-15 17:41:02 +0600

    academic semester : getAllSemester, getAndUpdateSemesterById, getSingleSemesterById

--- src/app/module/academicSemester/academicSemester.route.ts
+++ src/app/module/academicSemester/academicSemester.route.ts
@@ -8,5 +8,9 @@ const router = Router();
 //
 router.post('/create-academic-semester',validationRequest(AcademicSemesterValidations.createAcademicSemesterValidationSchema),AcademicSemesterControllers.createAcademicSemester);
 
+router.get('/',AcademicSemesterControllers.getAllAcademicSemesters),
+router.get('/:semesterId',AcademicSemesterControllers.getSingleAcademicSemester)
+router.patch('/:semesterId',validationRequest(AcademicSemesterValidations.createAcademicSemesterValidationSchema),AcademicSemesterControllers.updateSingleAcademicSemester)
+
 
 export const AcademicSemesterRoute = router;
