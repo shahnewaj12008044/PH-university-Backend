ab4733a687e9e0232e7a15041a64097d2cb7029f
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-06-16 11:56:12 +0600

    generatedStudentId bug fixed

--- src/app/module/user/user.utils.ts
+++ src/app/module/user/user.utils.ts
@@ -6,12 +6,23 @@ const findLastStudentId = async () => {
   const lastStudent = await User.findOne({ role: 'student' }, { id: 1, _id: 0 })
     .sort({ createdAt: -1 })
     .lean();
-  return lastStudent?.id ? lastStudent.id.substring(6) : undefined;
+  return lastStudent?.id ? lastStudent.id : undefined;
 };
 
 //set manually id
 export const generateStudentId = async (payload: TAcademicSemester) => {
-  const currentId = (await findLastStudentId()) || (0).toString();
+  let currentId = (0).toString();
+  const lastStudentId = await findLastStudentId();
+  const lastStudentIdCode = lastStudentId?.substring(4,6);
+  const lastStudentIdYear = lastStudentId?.substring(0,4);
+  const currentSemester = payload.code;
+  const currentYear = payload.year;
+
+  if(lastStudentId && lastStudentIdYear === currentYear && lastStudentIdCode === currentSemester){
+    currentId  = lastStudentId.substring(6)
+  }
+
+ 
   let increamentId = (Number(currentId) + 1).toString().padStart(4, '0');
   increamentId = `${payload.year}${payload.code}${increamentId}`;
   return increamentId;
