374cca9057c8d62e48874a449d75d4adde3c783a
Md Shahnewaj Al Hasan <mdshahnewajalhasan@gmail.com>
2024-07-14 05:15:56 +0600

    Uncaught exception error and unhandled rejection error done

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,34 @@
-import express, { Application, Request, Response } from "express"
-import cors from "cors"
-import globalErrorHandler from "./app/middlewares/globalErrorHandler"
-import notFound from "./app/middlewares/notFound"
-import router from "./app/routes"
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
+import notFound from './app/middlewares/notFound';
+import router from './app/routes';
 
 
-const app : Application = express()
+const app: Application = express();
+
 
 //parsers
-app.use(express.json())
-app.use(cors())
+app.use(express.json());
+app.use(cors());
 
 //application routes
-app.use('/api/v1',router);
+app.use('/api/v1', router);
 
+
+const test = async (req: Request, res: Response) => {
+// const a = 10;
+//   res.send(a.toString());
+  Promise.reject() 
+};
+
+app.get('/', test);
 
 //Global error handler:
 app.use(globalErrorHandler);
 
 //not found
-app.use(notFound)
-
-
-app.get('/', (req: Request, res:Response) => {
-  res.send('Hello World!')
-})
+app.use(notFound);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,14 @@
 import mongoose from "mongoose";
 import app from "./app";
 import config from "./app/config";
+import { Server } from 'http';
+
+let server  : Server;
 
 async function main() {
     try{
         await mongoose.connect(config.db_url as string);
-        app.listen(config.port, () => {
+        server = app.listen(config.port, () => {
             console.log(`Example app listening on port ${config.port}`)
           })
     }catch(err){
@@ -14,3 +17,17 @@ async function main() {
     }
   }
   main()
+  process.on('unhandledRejection',()=>{
+    console.log(` unhandled rejection is detected! shutting down...`)
+    if(server){
+        server.close(()=>{
+            process.exit(1)
+        })
+    }
+    process.exit(1)
+  })
+
+  process.on('uncaughtException',()=>{
+    console.log(`uncaught exception detected. server is shutting down....`)
+    process.exit(1)
+  })
